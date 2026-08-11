1f36a6c0d28688d8eab7d415c6002abe3256c64f
ivalisha <ivalisha@proton.me>
2026-08-11 18:55:44 +0300

    connected electron and react and build app with vite

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+           Count is {count}
         </button>
       </section>
 
